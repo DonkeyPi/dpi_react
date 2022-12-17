4fdaed310c23003a910251ef9c13e70ac0032c4c
Samuel Ventura <samuel.ventura@yeico.com>
2022-12-16 21:48:05 -0600

    Full refresh with sys event. Initial term server. Eval catches raises.

--- lib/macros.ex
+++ lib/macros.ex
@@ -12,6 +12,7 @@ defmodule Ash.React.Macros do
       import Ash.React.Helpers
       alias Ash.React.App
 
+      def run(func, opts), do: App.run(func, nil, opts)
       def run(func, onevt, opts), do: App.run(func, onevt, opts)
 
       def child_spec(opts) do
